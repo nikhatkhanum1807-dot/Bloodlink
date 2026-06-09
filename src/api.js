c3130a708a6354aec7e016bec5a412a0d1183e69
nikhatkhanum1807-dot <nikhatkhanum1807@gmail.com>
2026-06-09 21:54:05 +0530

    fix production API baseURL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 // Automatically attach token to every request
